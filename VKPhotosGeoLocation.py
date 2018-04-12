ad0f84b83ad014a7147620df4b25df2f02b147e8
SecretRoot <nikolas270699@gmail.com>
2018-04-12 19:20:34 +0300

    Commit 2

--- VKPhotosGeoLocation.py
+++ VKPhotosGeoLocation.py
@@ -2,13 +2,13 @@ import vk
 
 print('VK Photos geo location')
 
-session = vk.Session('331a225e8827abf0e50bc6af39b39a298677dea9aef8a9bbf3a012f393220209a79275aa17f98e9166e64')
+session = vk.Session('89f4ba7a192cac2a979028f219b7ce2d020481bf829ec828ed041be0a53b8571493c551c96ba468366a6c')
 
 api = vk.API(session, v='5.74')
 
 friends = api.friends.get()
 
-print(len(friends))
-
-print(friends)
+friends_info = api.users.get(user_ids=friends)
+for friend in friends_info:
+    print('ID: %s Имя: %s %s' % (friend['uid'], friend['last_name'], friend['first_name']))
 
