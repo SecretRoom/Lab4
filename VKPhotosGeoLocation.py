5d00f925c620d93d55c7b248c4fefb4891233b80
SecretRoot <nikolas270699@gmail.com>
2018-04-13 18:24:51 +0300

    Commit 3

--- VKPhotosGeoLocation.py
+++ VKPhotosGeoLocation.py
@@ -1,10 +1,11 @@
 import vk
 import time
+
 print('VK Photos geo location')
 
-session = vk.Session(access_token='4daf29f676dfa2d591bb421cfccc8e4224bf0a623a67b7a3e190ef7650af0e79a1db543ba148320ace597')
+session = vk.Session('7bf78bd5772147c7164d663ed70bd03369c82251bb14fdb703df7f1311aba0c77b6ec8e46f82357ae75e8')
 
-api = vk.API(session, v='5.74')
+api = vk.API(session, v='4.104')
 
 friends = api.friends.get()
 
@@ -17,16 +18,18 @@ geolocation = []
 
 for id in friends:
     print('Получаем данные пользователя: %s' % id)
-    albums = api.photos.getAlbums(owner_id=1)
+    albums = api.photos.getAlbums(owner_id=id)
     print('\t...альбомов %s...' % len(albums))
     for album in albums:
         try:
-            photos = api.photos.get(ownerid=id, AlbumIds=album['aid'])
+            photos = api.photos.get(owner_id=id, album_id=album['aid'])
             print('\t\t...обрабатываем фотографии альбома...')
             for photo in photos:
-                if 'lat' in photo and 'long' in photo:
-                    geolocation.append((photo['lat'], photo['long']))
-            print('\t\t...найдено %s фото...' % len(photos))
+                try:
+                    if 'lat' in photo and 'long' in photo:
+                        geolocation.append((photo['lat'], photo['long']))
+                except:
+                    print('\t\t...найдено %s фото...' % len(photos))
         except:
             pass
         time.sleep(0.5)
@@ -36,7 +39,8 @@ js_code = ""
 for loc in geolocation:
     js_code += 'new google.maps.Marker({ position: {lat: %s, lng %s}, map: map});\n' % (loc[0], loc[1])
 html = open('map.html').read()
-html = html.replace('/AIzaSyDhYgKvkA76qy90g4GIAWz22l7xBF5PZG0', js_code)
+html = html.replace('/* PLACEHOLDER */', js_code)
 f = open('VKPhotoGeoLocation.html', 'w')
 f.write(html)
 f.close()
+
